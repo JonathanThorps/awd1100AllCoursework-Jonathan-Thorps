af3cdb0f46339430bce335dba1146637b8b14e95
JonathanThorps <jonathan_thorps@insideranken.org>
2022-01-10 08:13:57 -0600

    CH02
    Chapter 02 Labs

---
+++ Lab/CH02/CH02/Lab3/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Console.WriteLine("What is your age?");
+
+int age = Convert.ToInt32(Console.ReadLine());
+
+int daysOld = age * 365;
+
+Console.WriteLine("User is {0} years old, which is {1} days old.", age, daysOld);
