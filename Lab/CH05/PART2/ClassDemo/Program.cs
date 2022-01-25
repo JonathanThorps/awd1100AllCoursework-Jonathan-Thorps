f6e777c011d9303e280901fef68d6f22f2396271
JonathanThorps <jonathan_thorps@insideranken.org>
2022-01-25 09:40:32 -0600

    CH05 part 2
    CH05 Labs

---
+++ Lab/CH05/PART2/ClassDemo/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+int i = 1;
+
+do
+{
+    Console.WriteLine("Number {0}", i);
+
+    i+=2;
+
+}while (i < 10) ;
+
+Console.WriteLine("Thank you for using my program!");
---
+++ Lab/CH05/PART2/LAB5/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Please Enter a Test Score");
+Console.ReadLine();
+
+double testScore = 0;
+int numScores = 0;
+int minScore = int.MaxValue;
+int maxScore = int.MinValue;
+
+for (; ; )
+{
+    if (testScore > 100 || testScore < 0)
+        Console.Write("Enter a Valid Score");
+    if (testScore == 999) break;
+}
