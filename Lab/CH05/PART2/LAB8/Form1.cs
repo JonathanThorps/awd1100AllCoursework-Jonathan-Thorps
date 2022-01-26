df4882eff1480fa38e7f1efc36a7d0a08fb3bdfa
JonathanThorps <jonathan_thorps@insideranken.org>
2022-01-26 08:12:01 -0600

    Update Form1.cs

--- Lab/CH05/PART2/LAB8/Form1.cs
+++ Lab/CH05/PART2/LAB8/Form1.cs
@@ -16,20 +16,27 @@ namespace LAB8
         {
             InitializeComponent();
         }
+        decimal sales = Convert.ToDecimal((saleBox.Text));
 
         private void danielleBtn_Click(object sender, EventArgs e)
         {
-
+            decimal daniSales = 0;
+            decimal daniTotal = daniSales + sales;
+            daniSalesBox.Text = String.Format("{0:C}", daniSales);
         }
 
         private void edwardBtn_Click(object sender, EventArgs e)
         {
-
+            decimal edSales = 0; 
+            decimal edTotal = edSales + sales;
+            edSalesBox.Text = String.Format("{0:C}", edSales);
         }
 
         private void francisBtn_Click(object sender, EventArgs e)
         {
-
+            decimal frankSales = 0;
+            decimal frankTotal = frankSales + sales;
+            frankSalesBox.Text = String.Format("{0:C}", frankSales);
         }
     }
 }
