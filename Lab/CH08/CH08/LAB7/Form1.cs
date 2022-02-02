48e7a143fb54e79b2198602042b3ac1ff4d95f0a
JonathanThorps <jonathan_thorps@insideranken.org>
2022-02-02 09:25:49 -0600

    Update Form1.cs

--- Lab/CH08/CH08/LAB7/Form1.cs
+++ Lab/CH08/CH08/LAB7/Form1.cs
@@ -30,6 +30,38 @@ namespace LAB7
                 movie[3] = "Lethal Weapon";
                 movie[4] = "Invasion U.S.A.";
             }
+            string[] director = new string[5];
+            {
+                director[0] = "John Woo";
+                director[1] = "George P. Cosmatos";
+                director[2] = "John McTiernan";
+                director[3] = "Richard Donner";
+                director[4] = "Joseph Zito";
+            }
+            string[] description = new string[5];
+            {
+                description[0] = "A hitman seeks redemption";
+                description[1] = "Former soldier is called to action by his country";
+                description[2] = "An alien hunter seeks elite prey";
+                description[3] = "Cop gets new crazy partner to take down mercenaries";
+                description[4] = "One man army seeks to stop invasion of America";
+            }
+            for (int i = 0; i < movie.Length || i < director.Length; i++)
+                {
+                if (movie[i].ToLower().Contains(searchTerms.ToLower()) || director[i].ToLower().Contains(searchTerms.ToLower()))
+                {
+                    lblTitleOut.Text = movie[i];
+                    lblDirectorOut.Text = director[i];
+                    lblDescription.Text = description[i];
+                    break;
+                }
+                else
+                {
+                    lblTitleOut.Text = searchError;
+                    lblDirectorOut.Text = searchError;
+                    lblDescription.Text = searchError;
+                }
+            }
         }
     }
 }
