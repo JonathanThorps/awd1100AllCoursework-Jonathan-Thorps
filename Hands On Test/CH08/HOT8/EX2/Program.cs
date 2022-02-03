33ca0588f171dd19b96a0bd76f3c9772f9749261
JonathanThorps <jonathan_thorps@insideranken.org>
2022-02-03 11:50:24 -0600

    HOT8

---
+++ Hands On Test/CH08/HOT8/EX2/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Enter price of shopping cart items or press [0] to quit");
+
+for (int i = 1; i < 10 ; i++)
+{
+    decimal subtotal = 0;
+    const decimal tax = 0.08m;
+    int numItems = 0;
+    decimal itemPrice = Convert.ToDecimal(Console.ReadLine());
+
+    if (itemPrice > 0)
+    {
+        subtotal = subtotal + itemPrice;
+        numItems += i;
+    }
+
+    if(itemPrice == 0)
+    {
+        Console.WriteLine("Items Purchased {0}", numItems);
+        Console.WriteLine("Average Price {0:C}", subtotal / numItems);
+        Console.WriteLine("SubTotal: {0:C}", subtotal);
+        Console.WriteLine("Tax: {0:C}", tax * subtotal);
+        Console.WriteLine("Total: {0:C}", (subtotal * tax) + subtotal);
+        break;
+    }
+    
+    else if (itemPrice < 0)
+    {
+        Console.WriteLine("Invalid Price!");
+    }
+}
+
+
+
+
+
