f3ece3d98843a07b0b2cfa609a5b42122214ed61
JonathanThorps <jonathan_thorps@insideranken.org>
2022-02-08 11:51:10 -0600

    CH6 Labs

--- Lab/CH06/CH6/LAB4/Form1.cs
+++ Lab/CH06/CH6/LAB4/Form1.cs
@@ -16,23 +16,25 @@ namespace LAB4
         public Form1()
         {
             InitializeComponent();
-            decimal gradePointAverage = Convert.ToDecimal(gpaScoreBox.Text);
-            decimal admitTestScore = Convert.ToDecimal(testScoreBox.Text);
-
+            
         }
 
         private void admitBtn_Click(object sender, EventArgs e)
         {
+                    
             IsStudentAccepted();
 
         }
         private void IsStudentAccepted()
         {
-            if (gradePointAverage > 3 && admitTestScore > 60)
+            decimal gradePointAverage = Convert.ToDecimal(gpaScoreBox.Text);
+            decimal admitTestScore = Convert.ToDecimal(testScoreBox.Text);
+
+            if (gradePointAverage >= 3 && admitTestScore >= 60)
             {
             lblAdmitOutput.Text = "Accept";
             }
-            else if (gradePointAverage < 3 && admitTestScore > 80)
+            else if (gradePointAverage < 3 && admitTestScore >= 80)
             {
             lblAdmitOutput.Text = "Accept";
             }
