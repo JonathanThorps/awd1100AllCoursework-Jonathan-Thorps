7d2777f908f4d7a4508bf2eb8fe852e46f371a03
JonathanThorps <jonathan_thorps@insideranken.org>
2022-02-08 08:06:11 -0600

    CH6 Lab

---
+++ Lab/CH06/CH6/LAB4/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LAB4
+{
+    public partial class Form1 : Form
+    {
+       
+        public Form1()
+        {
+            InitializeComponent();
+            decimal gradePointAverage = Convert.ToDecimal(gpaScoreBox.Text);
+            decimal admitTestScore = Convert.ToDecimal(testScoreBox.Text);
+
+        }
+
+        private void admitBtn_Click(object sender, EventArgs e)
+        {
+            IsStudentAccepted();
+
+        }
+        private void IsStudentAccepted()
+        {
+            if (gradePointAverage > 3 && admitTestScore > 60)
+            {
+            lblAdmitOutput.Text = "Accept";
+            }
+            else if (gradePointAverage < 3 && admitTestScore > 80)
+            {
+            lblAdmitOutput.Text = "Accept";
+            }
+            else
+            {
+            lblAdmitOutput.Text = "Reject";
+            }
+        }
+
+            
+    }
+}
