09b895c68f243beab65c3702d322ab3f7d52151b
JonathanThorps <jonathan_thorps@insideranken.org>
2022-02-16 01:00:47 -0600

    HOT6 retake

---
+++ Hands On Test/CH06/HOT6/HOT6 Retake/Form1.Designer.cs
@@ -0,0 +1,259 @@
+﻿namespace HOT6_Retake
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtTypeBox = new System.Windows.Forms.TextBox();
+            this.txtNumBox = new System.Windows.Forms.TextBox();
+            this.txtKeywordBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblPokemon = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.lblEntryDetail = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.lblType = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.btnTypeSearch = new System.Windows.Forms.Button();
+            this.btnNumSearch = new System.Windows.Forms.Button();
+            this.btnKeywordSearch = new System.Windows.Forms.Button();
+            this.lblPokedexNum = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lblPokedexNum);
+            this.groupBox1.Controls.Add(this.btnKeywordSearch);
+            this.groupBox1.Controls.Add(this.btnNumSearch);
+            this.groupBox1.Controls.Add(this.btnTypeSearch);
+            this.groupBox1.Controls.Add(this.label10);
+            this.groupBox1.Controls.Add(this.lblType);
+            this.groupBox1.Controls.Add(this.label8);
+            this.groupBox1.Controls.Add(this.lblEntryDetail);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.lblPokemon);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.txtKeywordBox);
+            this.groupBox1.Controls.Add(this.txtNumBox);
+            this.groupBox1.Controls.Add(this.txtTypeBox);
+            this.groupBox1.Location = new System.Drawing.Point(102, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(594, 505);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Pokedex";
+            // 
+            // txtTypeBox
+            // 
+            this.txtTypeBox.Location = new System.Drawing.Point(152, 35);
+            this.txtTypeBox.Name = "txtTypeBox";
+            this.txtTypeBox.Size = new System.Drawing.Size(301, 27);
+            this.txtTypeBox.TabIndex = 0;
+            // 
+            // txtNumBox
+            // 
+            this.txtNumBox.Location = new System.Drawing.Point(152, 86);
+            this.txtNumBox.Name = "txtNumBox";
+            this.txtNumBox.Size = new System.Drawing.Size(301, 27);
+            this.txtNumBox.TabIndex = 1;
+            // 
+            // txtKeywordBox
+            // 
+            this.txtKeywordBox.Location = new System.Drawing.Point(152, 137);
+            this.txtKeywordBox.Name = "txtKeywordBox";
+            this.txtKeywordBox.Size = new System.Drawing.Size(301, 27);
+            this.txtKeywordBox.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(29, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Type";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(29, 89);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(122, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Pokedex Number";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(29, 144);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(67, 20);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Keyword";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(30, 193);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(49, 20);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Name";
+            // 
+            // lblPokemon
+            // 
+            this.lblPokemon.AutoSize = true;
+            this.lblPokemon.Location = new System.Drawing.Point(29, 235);
+            this.lblPokemon.Name = "lblPokemon";
+            this.lblPokemon.Size = new System.Drawing.Size(0, 20);
+            this.lblPokemon.TabIndex = 7;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(29, 276);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(42, 20);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "Entry";
+            // 
+            // lblEntryDetail
+            // 
+            this.lblEntryDetail.AutoSize = true;
+            this.lblEntryDetail.Location = new System.Drawing.Point(29, 313);
+            this.lblEntryDetail.Name = "lblEntryDetail";
+            this.lblEntryDetail.Size = new System.Drawing.Size(0, 20);
+            this.lblEntryDetail.TabIndex = 9;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(30, 355);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(40, 20);
+            this.label8.TabIndex = 10;
+            this.label8.Text = "Type";
+            // 
+            // lblType
+            // 
+            this.lblType.AutoSize = true;
+            this.lblType.Location = new System.Drawing.Point(30, 396);
+            this.lblType.Name = "lblType";
+            this.lblType.Size = new System.Drawing.Size(0, 20);
+            this.lblType.TabIndex = 11;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(30, 431);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(122, 20);
+            this.label10.TabIndex = 12;
+            this.label10.Text = "Pokedex Number";
+            // 
+            // btnTypeSearch
+            // 
+            this.btnTypeSearch.Location = new System.Drawing.Point(471, 34);
+            this.btnTypeSearch.Name = "btnTypeSearch";
+            this.btnTypeSearch.Size = new System.Drawing.Size(94, 29);
+            this.btnTypeSearch.TabIndex = 13;
+            this.btnTypeSearch.Text = "Search";
+            this.btnTypeSearch.UseVisualStyleBackColor = true;
+            this.btnTypeSearch.Click += new System.EventHandler(this.btnTypeSearch_Click);
+            // 
+            // btnNumSearch
+            // 
+            this.btnNumSearch.Location = new System.Drawing.Point(471, 86);
+            this.btnNumSearch.Name = "btnNumSearch";
+            this.btnNumSearch.Size = new System.Drawing.Size(94, 29);
+            this.btnNumSearch.TabIndex = 14;
+            this.btnNumSearch.Text = "Search";
+            this.btnNumSearch.UseVisualStyleBackColor = true;
+            this.btnNumSearch.Click += new System.EventHandler(this.btnNumSearch_Click);
+            // 
+            // btnKeywordSearch
+            // 
+            this.btnKeywordSearch.Location = new System.Drawing.Point(471, 135);
+            this.btnKeywordSearch.Name = "btnKeywordSearch";
+            this.btnKeywordSearch.Size = new System.Drawing.Size(94, 29);
+            this.btnKeywordSearch.TabIndex = 15;
+            this.btnKeywordSearch.Text = "Search";
+            this.btnKeywordSearch.UseVisualStyleBackColor = true;
+            this.btnKeywordSearch.Click += new System.EventHandler(this.btnKeywordSearch_Click);
+            // 
+            // lblPokedexNum
+            // 
+            this.lblPokedexNum.AutoSize = true;
+            this.lblPokedexNum.Location = new System.Drawing.Point(30, 472);
+            this.lblPokedexNum.Name = "lblPokedexNum";
+            this.lblPokedexNum.Size = new System.Drawing.Size(0, 20);
+            this.lblPokedexNum.TabIndex = 16;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 532);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private GroupBox groupBox1;
+        private Label lblPokedexNum;
+        private Button btnKeywordSearch;
+        private Button btnNumSearch;
+        private Button btnTypeSearch;
+        private Label label10;
+        private Label lblType;
+        private Label label8;
+        private Label lblEntryDetail;
+        private Label label6;
+        private Label lblPokemon;
+        private Label label4;
+        private Label label3;
+        private Label label2;
+        private Label label1;
+        private TextBox txtKeywordBox;
+        private TextBox txtNumBox;
+        private TextBox txtTypeBox;
+    }
+}
---
+++ Hands On Test/CH06/HOT6/HOT6 Retake/Form1.cs
@@ -0,0 +1,131 @@
+namespace HOT6_Retake
+{
+    public partial class Form1 : Form
+    {
+        string[] pokemon = new string[6]
+        {
+            "Pikachu",
+            "Butterfree",
+            "Pidgeotto",
+            "Bulbasaur",
+            "Charmander",
+            "Squirtle"
+        };
+        string[] pokemonType = new string[6]
+        {
+            "Electric",
+            "Bug",
+            "Flying",
+            "Grass",
+            "Fire",
+            "Water"
+        };
+        int[] pokedexNumber = new int[6]
+        {
+            025,
+            012,
+            017,
+            001,
+            004,
+            007
+        };
+        string[] description = new string[6]
+        {
+            "The Mouse Pokemon. It can generate electric attacks from the electric pouches located in both of its cheeks.",
+            "The Butterfly Pokemon. It's love of flower nectar drows it to seek out flowers, even if they don't have much pollen.",
+            "The Bird Pokemon. It is armed with sharp claws and dives from the sky to capture it's prey. Unlike the more gentle Pidgey, it can be dangerous. Approach with extreme caution.",
+            "It bears the seed of a plant on it's back from birth. The seed slowly develops. Researchers are unsure whether to classify them as plants or animals. They are extremely tough and very difficult to capture in the wild.",
+            "A flame burns on the tip of it's tail from birth. It is said that a Charmander dies if it's flame ever goes out.",
+            "This Tiny Turtle Pokemon draws it's long neck into it's shell to launch an incredible water attack with amazing range and accuracy. The blasts can be quite powerful."
+        };
+        string errorMsg = "Pokemon not found";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnTypeSearch_Click(object sender, EventArgs e)
+        {
+            string pokemonType = txtTypeBox.Text;
+            ShowPokemon();
+            SearchByType(pokemonType);
+        }
+
+        private void btnNumSearch_Click(object sender, EventArgs e)
+        {
+            int pokedexNum = Convert.ToInt32(txtNumBox.Text);
+            ShowPokemon();
+            SearchByPokedexNumber(pokedexNum);
+        }
+
+        private void btnKeywordSearch_Click(object sender, EventArgs e)
+        {
+            string keyword = txtKeywordBox.Text;
+            ShowPokemon();
+            SearchByKeyword(keyword);
+        }
+        private int SearchByType(string type)
+        {
+            for (int i = 0; i < pokemonType.Length; i++)
+            {
+                if (type.ToLower().Contains(pokemonType[i].ToLower()))
+                {
+                    return pokemonType[i];
+                }
+                else
+                {
+                    lblPokemon.Text = errorMsg;
+                    lblEntryDetail.Text = errorMsg;
+                    lblType.Text = errorMsg;
+                    lblPokedexNum.Text = errorMsg;
+                }
+            }
+        }
+        private int SearchByPokedexNumber(int num)
+        {
+            for (int i = 0; i < pokedexNumber.Length; i++)
+            {
+                if (num == pokedexNumber[i])
+                {
+                    return i;
+                }
+                else
+                {
+                    lblPokemon.Text = errorMsg;
+                    lblEntryDetail.Text = errorMsg;
+                    lblType.Text = errorMsg;
+                    lblPokedexNum.Text = errorMsg;
+                }
+            }
+        }
+        private string SearchByKeyword(string keyword)
+        {
+            for (int i = 0;i < keyword.Length; i++)
+            {
+                if (keyword.ToLower().Contains(description[i].ToLower()))
+                {
+                    return i;
+                }
+                else 
+                {
+                    lblPokemon.Text = errorMsg;
+                    lblEntryDetail.Text = errorMsg;
+                    lblType.Text = errorMsg;
+                    lblPokedexNum.Text = errorMsg;
+                }
+            }
+            
+        }
+        private string ShowPokemon(string[] pokemon, string[] pokemonType,int pokedexNumber, string[] description )
+        {
+            if (SearchByType() == true || SearchByPokedexNumber() == true || SearchByKeyword() == true)
+            {
+                lblPokemon.Text = String.Format("{0}", pokemon[i]);
+                lblEntryDetail.Text = String.Format("{0}", description[i]);
+                lblType.Text = String.Format("{0}", pokemonType[i]);
+                lblPokedexNum.Text = String.Format("{0}", pokedexNumber[i]);
+            }
+
+        }
+    }
+}
