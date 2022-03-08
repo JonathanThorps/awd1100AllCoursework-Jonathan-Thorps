ba1a8ecd81f6c51b68eaae3705e265241b770e2d
JonathanThorps <jonathan_thorps@insideranken.org>
2022-03-08 09:41:28 -0600

    CH14

---
+++ Lab/CH14/ClassDemo/Person.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassDemo
+{
+    internal class Person
+    {
+        private string _firstName;
+        private string _lastName;
+
+        public Person()
+        {
+
+        }
+
+        public Person(string firstName, string lastName)
+        {
+            _firstName = FirstName;
+            _lastName = LastName;
+        }
+
+        public string FirstName
+            { get { return _firstName; } set { _firstName = value; } }
+
+        public string LastName
+            { get { return _lastName; } set { _lastName = value; } }
+    }
+}
