95233a4e6e546745c0676771e54ea33a42bc4b6a
JonathanThorps <jonathan_thorps@insideranken.org>
2022-03-17 11:59:31 -0500

    HOT6 Exam

---
+++ Hands On Test/CH06/SampleExam/SampleExam/Form1.Designer.cs
@@ -0,0 +1,243 @@
+﻿namespace SampleExam
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtNameSearch = new System.Windows.Forms.TextBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtNationSearch = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtNationality = new System.Windows.Forms.TextBox();
+            this.txtAgency = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtAgencySearch = new System.Windows.Forms.TextBox();
+            this.lblSearchError = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(88, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(142, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Search Double Agents";
+            // 
+            // txtNameSearch
+            // 
+            this.txtNameSearch.Location = new System.Drawing.Point(272, 22);
+            this.txtNameSearch.Name = "txtNameSearch";
+            this.txtNameSearch.Size = new System.Drawing.Size(280, 22);
+            this.txtNameSearch.TabIndex = 1;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(608, 52);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(106, 39);
+            this.btnSearch.TabIndex = 2;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(34, 50);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(210, 244);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(271, 50);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 16);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Name:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(271, 93);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(85, 16);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Nationality:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(271, 140);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(63, 16);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Agency:";
+            // 
+            // txtNationSearch
+            // 
+            this.txtNationSearch.Location = new System.Drawing.Point(272, 60);
+            this.txtNationSearch.Name = "txtNationSearch";
+            this.txtNationSearch.Size = new System.Drawing.Size(280, 22);
+            this.txtNationSearch.TabIndex = 8;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(88, 63);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(116, 16);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Search Nationality";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(399, 47);
+            this.txtName.Name = "txtName";
+            this.txtName.ReadOnly = true;
+            this.txtName.Size = new System.Drawing.Size(263, 22);
+            this.txtName.TabIndex = 10;
+            // 
+            // txtNationality
+            // 
+            this.txtNationality.Location = new System.Drawing.Point(399, 90);
+            this.txtNationality.Name = "txtNationality";
+            this.txtNationality.ReadOnly = true;
+            this.txtNationality.Size = new System.Drawing.Size(263, 22);
+            this.txtNationality.TabIndex = 11;
+            // 
+            // txtAgency
+            // 
+            this.txtAgency.Location = new System.Drawing.Point(399, 137);
+            this.txtAgency.Name = "txtAgency";
+            this.txtAgency.ReadOnly = true;
+            this.txtAgency.Size = new System.Drawing.Size(263, 22);
+            this.txtAgency.TabIndex = 12;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtName);
+            this.groupBox1.Controls.Add(this.txtNationality);
+            this.groupBox1.Controls.Add(this.txtAgency);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.pictureBox1);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Location = new System.Drawing.Point(52, 169);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(698, 333);
+            this.groupBox1.TabIndex = 14;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Dossier";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(88, 103);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(117, 16);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "Search by Agency";
+            // 
+            // txtAgencySearch
+            // 
+            this.txtAgencySearch.Location = new System.Drawing.Point(272, 100);
+            this.txtAgencySearch.Name = "txtAgencySearch";
+            this.txtAgencySearch.Size = new System.Drawing.Size(280, 22);
+            this.txtAgencySearch.TabIndex = 16;
+            // 
+            // lblSearchError
+            // 
+            this.lblSearchError.AutoSize = true;
+            this.lblSearchError.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSearchError.ForeColor = System.Drawing.Color.Red;
+            this.lblSearchError.Location = new System.Drawing.Point(287, 137);
+            this.lblSearchError.Name = "lblSearchError";
+            this.lblSearchError.Size = new System.Drawing.Size(248, 20);
+            this.lblSearchError.TabIndex = 17;
+            this.lblSearchError.Text = "***PERSON NOT FOUND!***";
+            this.lblSearchError.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 514);
+            this.Controls.Add(this.lblSearchError);
+            this.Controls.Add(this.txtAgencySearch);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtNationSearch);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.txtNameSearch);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtNameSearch;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtNationSearch;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtNationality;
+        private System.Windows.Forms.TextBox txtAgency;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtAgencySearch;
+        private System.Windows.Forms.Label lblSearchError;
+    }
+}
+
---
+++ Hands On Test/CH06/SampleExam/SampleExam/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SampleExam
+{
+    public partial class Form1 : Form
+    {
+        string[] doubleAgents = new string[7] { "Aldrich Ames", "Mathilde Carre", "Oleg Penkovsky", "Donald Mclean",
+                "Margaretha Geertrudia MacLeod", "Matei Pavel Haiduca", "Brian Nelson" };
+        string[] agencies = new string[7] { "CIA", "None", "GRU", "None", "None", "Department of State Security", "Ulster Defense Association" };
+        string[] nationalities = new string[7] { "American", "French", "Russian", "British", "Dutch", "Romanian", "Irish" };
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            string spyName = txtNameSearch.Text;
+            string spyNation = txtNationSearch.Text;
+            string spyAgency = txtAgencySearch.Text;
+
+            ShowDossier();
+        }
+        public void SearchByName(string spyName)
+        {
+            for (int i = 0; i < doubleAgents; i++)
+            {
+                if (string.IsNullOrEmpty(spyName))
+                {
+                    lblSearchError.Visible = true;
+                }
+                else if (string.ToLower().Contains(spyName))
+                {
+                    return;
+                }
+                else
+                {
+                    lblSearchError.Visible = true;
+                }
+            }
+        }
+        public void SearchByNationality(string spyNation)
+        {
+            for (int i = 0; i < nationality[i]; i++)
+            {
+                if (string.IsNullOrEmpty(spyNation))
+                {
+                    lblSearchError.Visible = true;
+                }
+                else if (string.ToLower().Contains(spyNation))
+                {
+                    return;
+                }
+                else
+                {
+                    lblSearchError.Visible = true;
+                }
+            }
+        }
+        public void SearchByAgency(string spyAgency)
+        {
+            for (int i = 0; i < agencies[i]; i++)
+            {
+                if (string.IsNullOrEmpty(spyAgency))
+                {
+                    lblSearchError.Visible = true;
+                }
+                else if (string.ToLower().Contains(spyAgency))
+                {
+                    return;
+                }
+                else
+                {
+                    lblSearchError.Visible = true;
+                }
+            }
+        }
+        public string ShowDossier()
+        {
+            txtName.Text = SearchByName();
+            txtNationality.Text = SearchByNationality();
+            txtAgency.Text = SearchByAgency();
+        }
+    }
+}
