a390908a0fe96421601778494858f0c997236467
JonathanThorps <jonathan_thorps@insideranken.org>
2022-03-29 11:51:18 -0500

    CH10 Labs

---
+++ Lab/CH10/LAB2/Industry.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB2
+{
+    public class Industry
+    {
+        private string _industry;
+        private Job[] _jobs;
+
+        public Industry()
+        {
+            _industry =
+        }
+    }
+}
---
+++ Lab/CH10/LAB2/Job.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LAB2
+{
+    public class Job
+    {
+        public Job(string JobTitle, LinkLabel link)
+        {
+            JobTitle = jobTitle;
+            Link = link;
+        }
+        public string JobTitle { get; set; }
+        public LinkLabel Link { get; set; }
+        public override string ToString()
+        {
+            return JobTitle;
+        }
+    }
+}
