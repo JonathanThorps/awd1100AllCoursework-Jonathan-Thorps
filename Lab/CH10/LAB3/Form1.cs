5bc88e23e80949ac473a3c1195304874302bc2e5
JonathanThorps <jonathan_thorps@insideranken.org>
2022-03-31 11:52:38 -0500

    CH10Labs

---
+++ Lab/CH10/LAB3/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LAB3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void AnyTextbox_Click(object sender, EventArgs e)
+        {
+            TextBox t = (TextBox)sender;
+            t.BackColor = Color.Yellow;
+        }
+
+        private void AnyTextbox_Focus(object sender, EventArgs e)
+        {
+            TextBox t = (TextBox)sender;
+            t.BackColor = Color.LightBlue;
+        }
+
+        private void AnyTextbox_Leave(object sender, EventArgs e)
+        {
+            TextBox t = (TextBox)sender;
+            t.BackColor = Color.LightGray;
+        }
+    }
+}
