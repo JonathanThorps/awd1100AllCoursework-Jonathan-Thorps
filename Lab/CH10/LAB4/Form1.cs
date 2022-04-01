2c71054476d5a3ab6b39797bd42d370f18eaba82
JonathanThorps <jonathan_thorps@insideranken.org>
2022-04-01 11:53:55 -0500

    CH10Labs

---
+++ Lab/CH10/LAB4/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LAB4
+{
+    public partial class Form1 : Form
+    {
+        List<Student> students = new List<Student>()
+        {
+            new Student("Joe","Louis","INF 2100",'C', 2.75),
+            new Student("Oscar","DeLaHoya","ACR 1000",'A',3.5),
+            new Student("Mike","Tyson","AMT 1000",'D',2.0),
+            new Student("Lennox","Lewis","HVAC 1000",'A',4),
+            new Student("Roy","Jones Jr.","AWD 1100",'A',3.5),
+        };
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSearchFirstName_Click(object sender, EventArgs e)
+        {
+            string firstName = txtFirstName.Text;
+
+            try
+            {
+                for (int i = 0; i < students.Count; i++)
+                {
+                    if (students[i].FirstName.ToLower().Contains(firstName))
+                    {
+                        lblStudentName.Text = students[i].FirstName + students[i].LastName;
+                        lblCourseName.Text = students[i].Course;
+                        lblCourseGrade.Text = Convert.ToString(students[i].Grade);
+                        lblCourseGpa.Text = Convert.ToString(students[i].Gpa);
+                    }
+                }
+            }
+            catch(StudentException)
+            {
+                lblStudentName.ForeColor = Color.Red;
+                lblStudentName.Text = "Student Not Found";
+            }
+        }
+
+        private void btnSearchLastName_Click(object sender, EventArgs e)
+        {
+            string lastName = txtLastName.Text;
+
+            try
+            {
+                for (int i = 0; i < students.Count; i++)
+                {
+                    if (students[i].LastName.ToLower().Contains(lastName))
+                    {
+                        lblStudentName.Text = students[i].FirstName + students[i].LastName;
+                        lblCourseName.Text = students[i].Course;
+                        lblCourseGrade.Text = Convert.ToString(students[i].Grade);
+                        lblCourseGpa.Text = Convert.ToString(students[i].Gpa);
+                    }
+                }
+            }
+
+            catch (StudentException)
+            {
+                lblStudentName.ForeColor= Color.Red;
+                lblStudentName.Text = "Student Not Found";
+            }
+        }
+
+        private void txtFirstName_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(Convert.ToInt32(e.KeyCode) == 13)
+            {
+                btnSearchFirstName.PerformClick();
+            }
+        }
+
+        private void txtLastName_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (Convert.ToInt32(e.KeyCode) == 13)
+            {
+                btnSearchLastName.PerformClick();
+            }
+        }
+    }
+}
---
+++ Lab/CH10/LAB4/Student.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB4
+{
+    public class Student
+    {
+        private string _firstName;
+        private string _lastName;
+        private string _course;
+        private char _grade;
+        private double _gpa;
+
+        public Student(string firstName, string lastName, string course, char grade, double gpa)
+        {
+            _firstName = firstName;
+            _lastName = lastName;
+            _course = course;
+            _grade = grade;
+            _gpa = gpa;
+
+            if (string.IsNullOrEmpty(_firstName))
+            {
+                throw new StudentException(string message)
+            }
+            if (string.IsNullOrEmpty(_lastName))
+            {
+                throw new StudentException(string message)
+            }
+        }
+        public string FirstName { get { return _firstName; } }
+        public string LastName { get { return _lastName; } }
+        public string Course { get { return _course; } }
+        public char Grade { get { return _grade; } }
+        public double Gpa { get { return _gpa; } }
+    }
+}
