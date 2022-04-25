ad38d8f7141c0392413975999b3c2c2a9cc22183
JonathanThorps <jonathan_thorps@insideranken.org>
2022-04-25 11:55:19 -0500

    Final Project

--- Final Project/FinalProject/AccessManager/Employee.cs
+++ Final Project/FinalProject/AccessManager/Employee.cs
@@ -15,6 +15,18 @@ namespace AccessManager
         private Image _image;
         Department _department;
 
-
+        public Employee(string firstName,string lastName,int employeeId,Image Image,Department department)
+        {
+            _firstName = firstName;
+            _lastName = lastName;
+            _employeeId = employeeId;
+            _image = Image;
+            _department = department;
+        }
+        public string FirstName { get { return _firstName; } set { _firstName = value; } }
+        public string LastName { get { return _lastName; } set { _lastName = value; } }
+        public int EmployeeId { get { return _employeeId; } set { _employeeId = value;} }
+        public Image Image { get { return _image; } set { _image = value; } }
+        public Department Department { get { return _department; } set { _department = value; } }
     }
 }
